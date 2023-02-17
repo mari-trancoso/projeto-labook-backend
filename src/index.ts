7670558ab9c40ef7d268b778ed44752d6a652671
mari-trancoso <mnegraotrancoso@gmail.com>
2023-02-17 11:16:54 -0300

    :sparkles: feat: Implementado o env, variável de ambiente.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import cors from 'cors'
 import { userRouter } from './router/userRouter'
 import { postRouter } from './router/postRouter'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`)
 })
 
 app.use("/users", userRouter)
